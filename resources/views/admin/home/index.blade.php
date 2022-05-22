54e7a7838c65a64c56289162b055458b90dd6af7
quangdv <quangdv001@gmail.com>
2022-05-22 14:12:31 +0700

    f

--- resources/views/admin/home/index.blade.php
+++ resources/views/admin/home/index.blade.php
@@ -29,80 +29,19 @@ Biss
     <!--begin::Dashboard-->
     <!--begin::Row-->
     <div class="row">
-        <div class="col-xl-4">
+        
+        <div class="col-xl-12">
             <div class="row">
-                <div class="col-12">
+                <div class="col-xl-3">
                     <div class="fb-page" data-href="https://www.facebook.com/marketingbiss" data-tabs="timeline"
-                        data-width="2000" data-height="" data-small-header="false" data-adapt-container-width="true"
-                        data-hide-cover="false" data-show-facepile="true">
-                        <blockquote cite="https://www.facebook.com/marketingbiss" class="fb-xfbml-parse-ignore"><a
-                                href="https://www.facebook.com/marketingbiss">Biss Agency</a></blockquote>
-                    </div>
+                                data-width="2000" data-height="" data-small-header="false" data-adapt-container-width="true"
+                                data-hide-cover="false" data-show-facepile="true">
+                                <blockquote cite="https://www.facebook.com/marketingbiss" class="fb-xfbml-parse-ignore"><a
+                                        href="https://www.facebook.com/marketingbiss">Biss Agency</a></blockquote>
+                            </div>
+        
                 </div>
-                <div class="col-12 mt-10">
-
-                    <script type='text/javascript'>
-                        ! function (c, d) {
-                            "use strict";
-                            var e = !1,
-                                n = !1;
-                            if (d.querySelector)
-                                if (c.addEventListener) e = !0;
-                            if (c.wp = c.wp || {}, !c.wp.receiveEmbedMessage)
-                                if (c.wp.receiveEmbedMessage = function (e) {
-                                        var t = e.data;
-                                        if (t)
-                                            if (t.secret || t.message || t.value)
-                                                if (!/[^a-zA-Z0-9]/.test(t.secret)) {
-                                                    for (var r, a, i, s = d.querySelectorAll('iframe[data-secret="' + t
-                                                            .secret + '"]'), n = d.querySelectorAll(
-                                                            'blockquote[data-secret="' + t.secret + '"]'), o = 0; o < n
-                                                        .length; o++) n[o].style.display = "none";
-                                                    for (o = 0; o < s.length; o++)
-                                                        if (r = s[o], e.source === r.contentWindow) {
-                                                            if (r.removeAttribute("style"), "height" === t.message) {
-                                                                if (1e3 < (i = parseInt(t.value, 10))) i = 1e3;
-                                                                else if (~~i < 200) i = 200;
-                                                                r.height = i
-                                                            }
-                                                            if ("link" === t.message)
-                                                                if (a = d.createElement("a"), i = d.createElement("a"),
-                                                                    a.href = r.getAttribute("src"), i.href = t.value, i
-                                                                    .host === a.host)
-                                                                    if (d.activeElement === r) c.top.location.href = t
-                                                                        .value
-                                                        }
-                                                }
-                                    }, e) c.addEventListener("message", c.wp.receiveEmbedMessage, !1), d
-                                    .addEventListener("DOMContentLoaded", t, !1), c.addEventListener("load", t, !1);
-
-                            function t() {
-                                if (!n) {
-                                    n = !0;
-                                    for (var e, t, r = -1 !== navigator.appVersion.indexOf("MSIE 10"), a = !!navigator
-                                            .userAgent.match(/Trident.*rv:11\./), i = d.querySelectorAll(
-                                                "iframe.wp-embedded-content"), s = 0; s < i.length; s++) {
-                                        if (!(e = i[s]).getAttribute("data-secret")) t = Math.random().toString(36)
-                                            .substr(2, 10), e.src += "#?secret=" + t, e.setAttribute("data-secret", t);
-                                        if (r || a)(t = e.cloneNode(!0)).removeAttribute("security"), e.parentNode
-                                            .replaceChild(t, e)
-                                    }
-                                }
-                            }
-                        }(window, document);
-
-                    </script><iframe sandbox="allow-scripts" security="restricted"
-                        src="https://bissbrand.com/dich-vu-thiet-ke-profile-cong-ty/embed/" width="100%" height="400"
-                        title="&#8220;Thiết kế profile công ty chuyên nghiệp&#8221; &#8212; Biss Brand" frameborder="0"
-                        marginwidth="0" marginheight="0" scrolling="no" class="wp-embedded-content"></iframe>
-
-                </div>
-            </div>
-
-        </div>
-        <div class="col-xl-8">
-            <div class="row">
-                <div class="col-xl-12">
+                <div class="col-xl-9">
                     <script type='text/javascript'>
                         ! function (c, d) {
                             "use strict";
@@ -161,7 +100,7 @@ Biss
 
             </div>
             <div class="row mt-4">
-                <div class="col-xl-6">
+                <div class="col-xl-4">
 
                     <script type='text/javascript'>
                         ! function (c, d) {
@@ -218,7 +157,7 @@ Biss
                         title="&#8220;Thiết kế hồ sơ năng lực&#8221; &#8212; Biss Brand" frameborder="0" marginwidth="0"
                         marginheight="0" scrolling="no" class="wp-embedded-content"></iframe>
                 </div>
-                <div class="col-xl-6">
+                <div class="col-xl-4">
                     <script type='text/javascript'>
                         ! function (c, d) {
                             "use strict";
@@ -274,6 +213,62 @@ Biss
                         height="400" title="&#8220;Thiết kế nhận diện thương hiệu&#8221; &#8212; Biss Brand"
                         frameborder="0" marginwidth="0" marginheight="0" scrolling="no"
                         class="wp-embedded-content"></iframe>
+                </div>
+                <div class="col-xl-4">
+                    <script type='text/javascript'>
+                        ! function (c, d) {
+                            "use strict";
+                            var e = !1,
+                                n = !1;
+                            if (d.querySelector)
+                                if (c.addEventListener) e = !0;
+                            if (c.wp = c.wp || {}, !c.wp.receiveEmbedMessage)
+                                if (c.wp.receiveEmbedMessage = function (e) {
+                                        var t = e.data;
+                                        if (t)
+                                            if (t.secret || t.message || t.value)
+                                                if (!/[^a-zA-Z0-9]/.test(t.secret)) {
+                                                    for (var r, a, i, s = d.querySelectorAll('iframe[data-secret="' + t
+                                                            .secret + '"]'), n = d.querySelectorAll(
+                                                            'blockquote[data-secret="' + t.secret + '"]'), o = 0; o < n
+                                                        .length; o++) n[o].style.display = "none";
+                                                    for (o = 0; o < s.length; o++)
+                                                        if (r = s[o], e.source === r.contentWindow) {
+                                                            if (r.removeAttribute("style"), "height" === t.message) {
+                                                                if (1e3 < (i = parseInt(t.value, 10))) i = 1e3;
+                                                                else if (~~i < 200) i = 200;
+                                                                r.height = i
+                                                            }
+                                                            if ("link" === t.message)
+                                                                if (a = d.createElement("a"), i = d.createElement("a"),
+                                                                    a.href = r.getAttribute("src"), i.href = t.value, i
+                                                                    .host === a.host)
+                                                                    if (d.activeElement === r) c.top.location.href = t
+                                                                        .value
+                                                        }
+                                                }
+                                    }, e) c.addEventListener("message", c.wp.receiveEmbedMessage, !1), d
+                                    .addEventListener("DOMContentLoaded", t, !1), c.addEventListener("load", t, !1);
+
+                            function t() {
+                                if (!n) {
+                                    n = !0;
+                                    for (var e, t, r = -1 !== navigator.appVersion.indexOf("MSIE 10"), a = !!navigator
+                                            .userAgent.match(/Trident.*rv:11\./), i = d.querySelectorAll(
+                                                "iframe.wp-embedded-content"), s = 0; s < i.length; s++) {
+                                        if (!(e = i[s]).getAttribute("data-secret")) t = Math.random().toString(36)
+                                            .substr(2, 10), e.src += "#?secret=" + t, e.setAttribute("data-secret", t);
+                                        if (r || a)(t = e.cloneNode(!0)).removeAttribute("security"), e.parentNode
+                                            .replaceChild(t, e)
+                                    }
+                                }
+                            }
+                        }(window, document);
+
+                    </script><iframe sandbox="allow-scripts" security="restricted"
+                        src="https://bissbrand.com/dich-vu-thiet-ke-profile-cong-ty/embed/" width="100%" height="400"
+                        title="&#8220;Thiết kế profile công ty chuyên nghiệp&#8221; &#8212; Biss Brand" frameborder="0"
+                        marginwidth="0" marginheight="0" scrolling="no" class="wp-embedded-content"></iframe>
                 </div>
             </div>
         </div>
